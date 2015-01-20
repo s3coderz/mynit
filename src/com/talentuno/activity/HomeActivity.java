a749e580e33aacd5ad2dc3488cb7e72165d7b80a
Nithin <nithin.warier@happiestminds.com>
2015-01-20 22:54:21 +0530

    Modified home screen. Added actionbar and the corresponding images

--- src/com/talentuno/activity/HomeActivity.java
+++ src/com/talentuno/activity/HomeActivity.java
@@ -1,152 +1,50 @@
 package com.talentuno.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.talentuno.fragments.ContactsFragment;
-import com.talentuno.fragments.HomeFragment;
-import com.talentuno.fragments.LicenceAgreementFragment;
-import com.talentuno.fragments.ProfileFragment;
-import com.talentuno.fragments.SettingsFragment;
-import com.talentuno.fragments.TrendingFragment;
 import com.talentuno.mynit.R;
 
-public class HomeActivity extends Activity implements OnItemClickListener {
+public class HomeActivity extends Activity {
 
-	Fragment homeFragment, trendingFragment, contactsFragment, profileFragment,
-			settingsFragment, licenceAgreementFragment, selectedFragment;
-	private DrawerLayout mDrawerLayout;
-	private ListView mDrawerList;
-	private int selectedPos = -1;
-	private ActionBarDrawerToggle mDrawerToggle;
-	private String[] mTitles = { "Home", "Profile", "Settings", "Trending",
-			"Licence Agreement", "Contacts" };
+	private static final String TAG = HomeActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_activity_home);
-		init();
-		initDrawer();
-		loadFagment(0);
+		setActionbar();
+		
+//		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+//
+//        // set a custom shadow that overlays the main content when the drawer opens
+//        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+//        // set up the drawer's list view with items and click listener
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+//                R.layout.drawer_list_item, mPlanetTitles));
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	}
 
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		getActionBar().setHomeButtonEnabled(true);
+	private void setActionbar() {
+		Log.d(TAG, "setActionbar()");
+		ActionBar actionbar = getActionBar();
+		// actionbar.set
+	}
 
-		// ActionBarDrawerToggle ties together the the proper interactions
-		// between the sliding drawer and the action bar app icon
-		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
-		mDrawerLayout, /* DrawerLayout object */
-		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
-		R.string.drawer_open, /* "open drawer" description for accessibility */
-		R.string.drawer_close /* "close drawer" description for accessibility */
-		) {
-			public void onDrawerClosed(View view) {
-//				 getActionBar().setTitle(selectedTitle);
-				// invalidateOptionsMenu(); // creates call to
-				// onPrepareOptionsMenu()
-			}
-
-			public void onDrawerOpened(View drawerView) {
-				// getActionBar().setTitle(mDrawerTitle);
-				// invalidateOptionsMenu(); // creates call to
-				// onPrepareOptionsMenu()
-			}
-		};
-		mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-		// if (savedInstanceState == null) {
-		// // selectItem(0);
-		// }
-
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.mynhome, menu);
+		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// The action bar home/up action should open or close the drawer.
-		// ActionBarDrawerToggle will take care of this.
-		if (mDrawerToggle.onOptionsItemSelected(item)) {
-			return true;
-		}
-		// Handle action buttons
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void initDrawer() {
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mDrawerList = (ListView) findViewById(R.id.left_drawer);
-		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
-				GravityCompat.START);
-		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-				R.layout.drawer_list_item, mTitles));
-		mDrawerList.setOnItemClickListener(this);
-	}
-
-	private void init() {
-		homeFragment = new HomeFragment();
-		trendingFragment = new TrendingFragment();
-		contactsFragment = new ContactsFragment();
-		profileFragment = new ProfileFragment();
-		settingsFragment = new SettingsFragment();
-		licenceAgreementFragment = new LicenceAgreementFragment();
-	}
-
-	String selectedTitle = "";
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-			long arg3) {
-		if (selectedPos == position)
-			return;
-		selectedTitle = mTitles[position];
-		getActionBar().setTitle(selectedTitle);
-		loadFagment(position);
-		mDrawerList.setItemChecked(position, true);
-		// setTitle(mPlanetTitles[position]);
-		mDrawerLayout.closeDrawer(mDrawerList);
-
-	}
-
-	private void loadFagment(int position) {
-		Fragment oldFrag = selectedFragment;
-		switch (position) {
-		case 0:
-			selectedFragment = homeFragment;
-			break;
-		case 1:
-			selectedFragment = profileFragment;
-			break;
-		case 2:
-			selectedFragment = settingsFragment;
-			break;
-		case 3:
-			selectedFragment = trendingFragment;
-			break;
-		case 4:
-			selectedFragment = licenceAgreementFragment;
-			break;
-		case 5:
-			selectedFragment = contactsFragment;
-			break;
-		}
-		FragmentTransaction ft = getFragmentManager().beginTransaction();
-		if (oldFrag != null) {
-			ft.remove(oldFrag);
-		}
-		ft.add(R.id.content_frame, selectedFragment);
-		ft.commit();
-	}
 }
